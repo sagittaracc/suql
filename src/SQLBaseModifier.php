8a6125c6383b0f756fc56f0c226bcadd087d3b2d
yriiarutiunian <yriiarutiunian@gmail.com>
2020-04-01 23:45:23 +0300

    Разделил классы с базовы набором модификаторов функций и кастомных

---
+++ src/SQLBaseModifier.php
@@ -0,0 +1,39 @@
+<?php
+class SQLBaseModifier
+{
+  public static function mod_asc(&$queryObject, $field) {
+    $queryObject['order'][] = [
+      'field' => $queryObject['select'][$field]['field'],
+      'direction' => 'asc',
+    ];
+  }
+
+  public static function mod_desc(&$queryObject, $field) {
+    $queryObject['order'][] = [
+      'field' => $queryObject['select'][$field]['field'],
+      'direction' => 'desc',
+    ];
+  }
+
+  public static function mod_group(&$queryObject, $field) {
+    $queryObject['group'][] = $queryObject['select'][$field]['field'];
+    if (!empty($queryObject['select'][$field]['modifier']['group']))
+    {
+      $group = $queryObject['select'][$field]['alias'];
+      $name = $queryObject['select'][$field]['modifier']['group'][0];
+      $queryObject['having'][] = "$group = $name";
+    }
+  }
+
+  public static function mod_count(&$queryObject, $field) {
+    $fieldName = $queryObject['select'][$field]['field'];
+    $aliasName = $queryObject['select'][$field]['alias'];
+
+    $queryObject['select']["count($fieldName)" . ($aliasName ? " as $aliasName" : '')] = [
+			'field' => "count($fieldName)",
+			'alias' => $aliasName,
+		];
+
+    unset($queryObject['select'][$field]);
+  }
+}
--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 require 'SuQLLog.php';
 require 'TuringMachine.php';
 require 'SuQLEntityHelper.php';
+require 'SQLBaseModifier.php';
 require 'SQLModifier.php';
 require 'SuQLHandler.php';
 require 'SQLBuilder.php';
