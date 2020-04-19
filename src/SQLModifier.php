a6196b38592a2b36b0b302d58c26c082e8499b3c
yriiarutiunian <yriiarutiunian@gmail.com>
2020-04-19 16:12:49 +0300

    delete datediff modifier cuz it depends on the builder

--- src/SQLModifier.php
+++ src/SQLModifier.php
@@ -2,7 +2,4 @@
 class SQLModifier extends SQLBaseModifier
 {
   // You can define your own modifiers here
-  public static function mod_datediff(&$queryObject, $field) {
-    parent::default_handler('datediff', $queryObject, $field);
-  }
 }
