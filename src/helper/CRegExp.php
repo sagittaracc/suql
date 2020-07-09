646bc60b8a76cc270f39bf94dc441de19240e077
yriiarutiunian <yriiarutiunian@gmail.com>
2020-07-09 10:45:12 +0300

    white space fixes

--- src/helper/CRegExp.php
+++ src/helper/CRegExp.php
@@ -2,57 +2,57 @@
 namespace Helper;
 
 class CRegExp {
-	private $regex;
-	private $flags;
-	protected $sequenceList = [];
+  private $regex;
+  private $flags;
+  protected $sequenceList = [];
 
-	function __construct($regex, $flags = '') {
-		if (count(explode('/', $regex)) > 1) {
-			$parts = explode('/', $regex);
-			$regex = $parts[1];
-			$flags = $parts[2];
-		}
+  function __construct($regex, $flags = '') {
+    if (count(explode('/', $regex)) > 1) {
+      $parts = explode('/', $regex);
+      $regex = $parts[1];
+      $flags = $parts[2];
+    }
 
-		$this->setRegex($regex);
-		$this->setFlags($flags);
-	}
+    $this->setRegex($regex);
+    $this->setFlags($flags);
+  }
 
-	public function setRegex($regex) {
-		$this->regex = $regex;
-		return $this;
-	}
+  public function setRegex($regex) {
+    $this->regex = $regex;
+    return $this;
+  }
 
-	public function setFlags($flags) {
-		$this->flags = $flags;
-		return $this;
-	}
+  public function setFlags($flags) {
+    $this->flags = $flags;
+    return $this;
+  }
 
-	public function registerSequence($name, $set) {
-		$sequence = new CRegSequence($name, $set);
+  public function registerSequence($name, $set) {
+    $sequence = new CRegSequence($name, $set);
 
-		if ($sequence->valid())
-			$this->sequenceList[$sequence->name] = $sequence->set;
+    if ($sequence->valid())
+      $this->sequenceList[$sequence->name] = $sequence->set;
 
-		return $this;
-	}
+    return $this;
+  }
 
-	private function buildRegex() {
-		return str_replace(
-			array_keys($this->sequenceList),
-			array_values($this->sequenceList),
-			"/{$this->regex}/{$this->flags}"
-		);
-	}
+  private function buildRegex() {
+    return str_replace(
+      array_keys($this->sequenceList),
+      array_values($this->sequenceList),
+      "/{$this->regex}/{$this->flags}"
+    );
+  }
 
-	public function match($subject) {
-		$regex = $this->buildRegex();
-		preg_match($regex, $subject, $matches);
-		return empty($matches) ? false : (isset($matches[1]) ? $matches[1] : true);
-	}
+  public function match($subject) {
+    $regex = $this->buildRegex();
+    preg_match($regex, $subject, $matches);
+    return empty($matches) ? false : (isset($matches[1]) ? $matches[1] : true);
+  }
 
-	public function match_all($subject) {
-		$regex = $this->buildRegex();
-		preg_match_all($regex, $subject, $matches);
-		return $matches;
-	}
+  public function match_all($subject) {
+    $regex = $this->buildRegex();
+    preg_match_all($regex, $subject, $matches);
+    return $matches;
+  }
 }
--- src/helper/CRegSequence.php
+++ src/helper/CRegSequence.php
@@ -2,15 +2,15 @@
 namespace Helper;
 
 class CRegSequence {
-	public $name;
-	public $set;
+  public $name;
+  public $set;
 
-	function __construct($name, $set) {
-		$this->name = "{:$name:}";
-		$this->set = '['.implode('', $set).']';
-	}
+  function __construct($name, $set) {
+    $this->name = "{:$name:}";
+    $this->set = '['.implode('', $set).']';
+  }
 
-	public function valid() {
-		return true;
-	}
+  public function valid() {
+    return true;
+  }
 }
