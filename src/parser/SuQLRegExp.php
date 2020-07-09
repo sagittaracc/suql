a21fcd06f7a2fa08e761fa8a7fca6cf30b19ace5
yriiarutiunian <yriiarutiunian@gmail.com>
2020-07-09 09:44:00 +0300

    white spaces

--- src/parser/SuQLRegExp.php
+++ src/parser/SuQLRegExp.php
@@ -2,9 +2,9 @@
 use Helper\CRegExp;
 
 class SuQLRegExp extends CRegExp {
-    function __construct($regex, $flags = '') {
-        parent::registerSequence('v', [SuQLSpecialSymbols::$prefix_declare_variable]);
-        parent::registerSequence('f', [SuQLSpecialSymbols::$prefix_declare_field_alias]);
-        parent::__construct($regex, $flags);
-    }
+  function __construct($regex, $flags = '') {
+    parent::registerSequence('v', [SuQLSpecialSymbols::$prefix_declare_variable]);
+    parent::registerSequence('f', [SuQLSpecialSymbols::$prefix_declare_field_alias]);
+    parent::__construct($regex, $flags);
+  }
 }
