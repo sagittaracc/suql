3c8011cbf6c769a74d30e8245f2272eb554f98c9
yriiarutiunian <yriiarutiunian@gmail.com>
2020-07-11 17:44:09 +0300

    added getQuery function

--- src/helper/SuQLObjectReader.php
+++ src/helper/SuQLObjectReader.php
@@ -5,4 +5,8 @@ class SuQLObjectReader {
   public static function getAllTheQueryList($osuql) {
     return array_keys($osuql['queries']);
   }
+
+  public static function getQuery(&$osuql, $name) {
+    return $osuql['queries'][$name];
+  }
 }
