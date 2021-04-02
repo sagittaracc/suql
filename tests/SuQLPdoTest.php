4a1aef4644ef54053cbedbb604bb5e6aa90f17f7
arutyunyan <arutyunyan@bolid.ru>
2021-04-02 17:37:49 +0300

    add an example of fetching data from two different databases at the same time

--- tests/SuQLPdoTest.php
+++ tests/SuQLPdoTest.php
@@ -2,6 +2,7 @@
 use core\SuQLSpecialSymbols;
 use PHPUnit\Framework\TestCase;
 use sagittaracc\model\UserDb;
+use sagittaracc\model\ProductDb;
 
 final class SuQLPdoTest extends TestCase
 {
@@ -11,7 +12,7 @@ final class SuQLPdoTest extends TestCase
 
     /**
      * Example of fetching from database
-     * 
+     *
      * $this->assertEquals(
      *   UserDb::db()->fetch(),
      *    [
@@ -19,5 +20,42 @@ final class SuQLPdoTest extends TestCase
      *    ]
      *  );
      */
+  }
+
+  public function testSelectFromTwoDifferentDatabases(): void
+  {
+    $this->assertTrue(true);
+
+    //
+    // UserDb gets the data from the test database (see the UserDb model)
+    //
+    // $this->assertEquals(
+    //   UserDb::db()->fetch(), [
+    //     0 => [
+    //       'id' => '1',
+    //       'login' => 'admin',
+    //       'password' => '123',
+    //       'role' => '1',
+    //       0 => '1',
+    //       1 => 'admin',
+    //       2 => '123',
+    //       3 => '1',
+    //     ]
+    //   ]
+    // );
+
+    //
+    // ProductDb gets the data from the store database (see the ProductDb model)
+    //
+    // $this->assertEquals(
+    //   ProductDb::db()->fetch(), [
+    //     0 => [
+    //       'id' => '1',
+    //       'name' => 'bottle',
+    //       0 => '1',
+    //       1 => 'bottle'
+    //     ]
+    //   ]
+    // );
   }
 }
