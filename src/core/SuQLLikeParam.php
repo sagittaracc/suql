f5da6eda1c356687fa8e95cd4d89683324898d2c
arutyunyan <arutyunyan@bolid.ru>
2021-04-22 09:56:20 +0300

    Ошибка с фильтрацией по оператору LIKE

--- src/core/SuQLLikeParam.php
+++ src/core/SuQLLikeParam.php
@@ -4,5 +4,16 @@ namespace core;
 
 class SuQLLikeParam extends SuQLParam
 {
-    
+    public function getParamList()
+    {
+        $paramList = parent::getParamList();
+
+        foreach ($paramList as $placeholder => &$value)
+        {
+            $paramList[$placeholder] = "%{$value}%";
+        }
+        unset($value);
+
+        return $paramList;
+    }
 }
