4314b6822f355a473230159fdacfd328f737ee6f
Yuriy Arutyunyan <sagittaracc@gmail.com>
2021-04-25 20:15:57 +0300

    working on phpdocs

--- src/core/SuQLUnion.php
+++ src/core/SuQLUnion.php
@@ -1,26 +1,53 @@
 <?php
+
 namespace suql\core;
 
 use suql\core\interface\UnionQueryInterface;
 
+/**
+ * Объект хранящий структуру union запроса
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
 class SuQLUnion extends SuQLQuery implements UnionQueryInterface
 {
-  private $suql   = '';
-
-  function __construct($osuql, $suql) {
-    parent::__construct($osuql);
-    $this->suql = $suql;
-  }
-
-  public function getSuQL() {
-    return $this->suql;
-  }
-
-  public function setSuQL($suql) {
-    $this->suql = $suql;
-  }
-
-  public function addUnionTable($unionType, $table) {
-    $this->suql .= " $unionType $table";
-  }
+    /**
+     * @var string $suql строковое представление union запроса @query1 union [all] @query2 union ...
+     */
+    private $suql = '';
+    /**
+     * Constructor
+     * @param suql\core\SuQLObject $osuql ссылка на основной объект OSuQL
+     * @param string $suql строковое представление union запроса @query1 union [all] @query2 union ...
+     */
+    function __construct($osuql, $suql)
+    {
+        parent::__construct($osuql);
+        $this->suql = $suql;
+    }
+    /**
+     * Получить union строку запроса
+     * @return string
+     */
+    public function getSuQL()
+    {
+        return $this->suql;
+    }
+    /**
+     * Задать union строку запроса
+     * @param string $suql
+     */
+    public function setSuQL($suql)
+    {
+        $this->suql = $suql;
+    }
+    /**
+     * Добавить таблицу или подзапрос к общей строке union запроса
+     * @param string|null $unionType может быть all
+     * @param string $table таблица или подзапрос в виде @subquery
+     */
+    public function addUnionTable($unionType, $table)
+    {
+        $this->suql .= " $unionType $table";
+    }
 }
