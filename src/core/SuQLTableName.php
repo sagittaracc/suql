ef7f15f3837ad6c1ab9b7e898c07d4d212e7caaf
Yuriy Arutyunyan <sagittaracc@gmail.com>
2021-04-25 20:19:53 +0300

    working on phpdocs

--- src/core/SuQLTableName.php
+++ src/core/SuQLTableName.php
@@ -1,4 +1,17 @@
 <?php
+
 namespace suql\core;
 
-class SuQLTableName extends SuQLName {}
+/**
+ * Обработчик названия таблицы
+ * Возможные варианты задания названия таблицы:
+ * 
+ *   1. table - строковое название таблицы
+ *   2. [table => alias] - таблица с заданием alias для нее
+ *   3. table@alias - строковый вариант задания таблицы с alias
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
+class SuQLTableName extends SuQLName
+{
+}
