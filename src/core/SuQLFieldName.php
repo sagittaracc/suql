62d86a996e03c47fd4c8c2d46d4c62e0f703893a
arutyunyan <arutyunyan@bolid.ru>
2021-04-27 17:06:21 +0300

    workin on phpdocs

--- src/core/SuQLFieldName.php
+++ src/core/SuQLFieldName.php
@@ -1,15 +1,35 @@
 <?php
+
 namespace suql\core;
 
-class SuQLFieldName extends SuQLName {
-  public $table;
-
-  function __construct($table, $name) {
-    $this->table = $table;
-    parent::__construct($name);
-  }
-
-  public function format($s) {
-    return str_replace(['%t', '%n', '%a'], [$this->table, $this->name, $this->alias], $s);
-  }
+/**
+ * Управление именем поля. Расширает функциона простого имени посредством добавления к имени еще и имени таблицы
+ * так как имя поля в базе данных определяется тремя параметрами: именем, таблицей и алиасом опционально
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
+class SuQLFieldName extends SuQLName
+{
+    /**
+     * @var string имя таблицы
+     */
+    public $table;
+    /**
+     * Constructor
+     * @param string $table имя таблицы
+     * @param string|array имя поля
+     */
+    function __construct($table, $name)
+    {
+        $this->table = $table;
+        parent::__construct($name);
+    }
+    /**
+     * Вывод имени поля в запрошенном формате. Помимо плейсхолдеров для имени и алиаса, также поддерживает %t - название таблицы
+     * @return string
+     */
+    public function format($s)
+    {
+        return str_replace(['%t', '%n', '%a'], [$this->table, $this->name, $this->alias], $s);
+    }
 }
