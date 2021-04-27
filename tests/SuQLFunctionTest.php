4e5acc858daf315632c86f91c6fb4dcf39788abf
arutyunyan <arutyunyan@bolid.ru>
2021-04-27 17:52:50 +0300

    update PlaceholderHelper

--- tests/SuQLFunctionTest.php
+++ tests/SuQLFunctionTest.php
@@ -7,23 +7,23 @@ final class SuQLFunctionTest extends TestCase
   public function testStoredProcedure(): void
   {
     $this->assertEquals(
-        SuQLProcedure::find('setSomething')->params(1, 'string', null)->getRawSql(),
-        "call setSomething(1,'string',NULL)"
+        SuQLProcedure::find('setSomething')->params(1, 'string', null, true, false)->getRawSql(),
+        "call setSomething(1,'string',NULL,true,false)"
     );
 
     $this->assertEquals(
-        SuQLFunction::find('getSomething')->params(1, 'string', null)->getRawSql(),
-        "select getSomething(1,'string',NULL)"
+        SuQLFunction::find('getSomething')->params(1, 'string', null, true, false)->getRawSql(),
+        "select getSomething(1,'string',NULL,true,false)"
     );
 
     $this->assertEquals(
-      SuQLProcedure::find('getSomething')->params(new SuQLPlaceholder('id'), 'string', null)->getRawSql(),
-      "call getSomething(:id,'string',NULL)"
+      SuQLProcedure::find('getSomething')->params(new SuQLPlaceholder('id'), 'string', null, true, false)->getRawSql(),
+      "call getSomething(:id,'string',NULL,true,false)"
     );
 
     $this->assertEquals(
-      SuQLFunction::find('getSomething')->params(new SuQLPlaceholder('id'), 'string', null)->getRawSql(),
-      "select getSomething(:id,'string',NULL)"
+      SuQLFunction::find('getSomething')->params(new SuQLPlaceholder('id'), 'string', null, true, false)->getRawSql(),
+      "select getSomething(:id,'string',NULL,true,false)"
     );
   }
 }
