26883e7a0b363b021f3ec573493bbee31606dfab
arutyunyan <arutyunyan@bolid.ru>
2021-04-27 15:44:33 +0300

    working on phpdocs

--- src/core/SuQLPlaceholder.php
+++ src/core/SuQLPlaceholder.php
@@ -2,15 +2,30 @@
 
 namespace suql\core;
 
+/**
+ * Обработка пользовательских плейсхолдеров
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
 class SuQLPlaceholder
 {
+    /**
+     * @var string $placeholder название плейсхолдера
+     */
     private $placeholder;
 
+    /**
+     * Constructor
+     * @param string $placeholder название плейсхолдера
+     */
     function __construct($placeholder)
     {
         $this->placeholder = $placeholder;
     }
-
+    /**
+     * Получить форматированное название плейсхолдера
+     * @return string форматированное название плейсхолдера
+     */
     public function getPlaceholder()
     {
         return ":$this->placeholder";
