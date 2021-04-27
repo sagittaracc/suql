4be9ff7f815d69a04bb5265344a9102e2672d311
arutyunyan <arutyunyan@bolid.ru>
2021-04-27 16:07:35 +0300

    working on phpdocs

--- src/core/SuQLQuery.php
+++ src/core/SuQLQuery.php
@@ -1,14 +1,33 @@
 <?php
+
 namespace suql\core;
 
-class SuQLQuery {
-  protected $osuql = null;
-
-  function __construct($osuql) {
-    $this->osuql = $osuql;
-  }
-
-  public function getOSuQL() {
-    return $this->osuql;
-  }
+/**
+ * Объект простого запроса хранящий ссылку на основной объект core\suql\SuQLObject
+ * Связующее звено конкретных реализаций запросов с основным объектом.
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
+abstract class SuQLQuery
+{
+    /**
+     * @var suql\core\SuQLObject ссылка на основной объект
+     */
+    protected $osuql = null;
+    /**
+     * Constructor
+     * @param suql\core\SuQLObject $osuql ссылка на основной объект
+     */
+    function __construct($osuql)
+    {
+        $this->osuql = $osuql;
+    }
+    /**
+     * Получает ссылку на основной объект
+     * @return suql\core\SuQLObject
+     */
+    public function getOSuQL()
+    {
+        return $this->osuql;
+    }
 }
