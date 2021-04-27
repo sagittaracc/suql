b43163ff58bdae612cfd9ac83a47b6ee10abdda4
arutyunyan <arutyunyan@bolid.ru>
2021-04-27 15:59:23 +0300

    working on phpdocs

--- src/core/SuQLFunction.php
+++ src/core/SuQLFunction.php
@@ -4,17 +4,35 @@ namespace suql\core;
 
 use sagittaracc\PlaceholderHelper;
 
+/**
+ * Управление хранимыми функциями
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
 class SuQLFunction extends SuQLQuery implements FunctionQueryInterface
 {
+    /**
+     * @var string название хранимой функции
+     */
     private $name;
+    /**
+     * @var array параметры хранимой функции
+     */
     private $params;
-
+    /**
+     * Constructor
+     * @param suql\core\SuQLObject $osuql ссылка на основной объект структуры запроса
+     * @param string $name название хранимой процедуры
+     */
     function __construct($osuql, $name)
     {
         parent::__construct($osuql);
         $this->name = $name;
     }
-
+    /**
+     * Добавление параметров в хранимую функцию
+     * @param array $param список параметров
+     */
     public function addParams($params)
     {
         foreach ($params as $param)
@@ -29,12 +47,18 @@ class SuQLFunction extends SuQLQuery implements FunctionQueryInterface
             }
         }
     }
-
+    /**
+     * Получает название хранимой функции
+     * @return string
+     */
     public function getName()
     {
         return $this->name;
     }
-
+    /**
+     * Получает список параметров
+     * @return array
+     */
     public function getParams()
     {
         return $this->params;
