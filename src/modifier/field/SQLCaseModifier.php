0549ffe83c7088501f4717c5e0b5b0d65585f883
arutyunyan <arutyunyan@bolid.ru>
2021-04-28 15:21:50 +0300

    working on phpdocs

--- src/modifier/field/SQLCaseModifier.php
+++ src/modifier/field/SQLCaseModifier.php
@@ -4,36 +4,43 @@ namespace suql\modifier\field;
 
 use sagittaracc\PlaceholderHelper;
 
+/**
+ * Модификатор case when
+ * Пример:
+ * 
+ * public static function mod_role($ofield, $params) {
+ *     self::mod_case([
+ *         (new SuQLModifier('between', [1, 2]))->applyTo($ofield->getField()) => new SuQLFieldName(<table>, <name>)
+ *         '$ = 1'   => 'admin',
+ *         '$ = 2'   => 'user',
+ *         '$ = 3'   => 'guest',
+ *         'default' => '',
+ *     ], $ofield, $params);
+ * }
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
 class SQLCaseModifier
 {
-  public static function mod_case($case, $ofield, $params) {
-    $fieldName = $ofield->getField();
-    $caseList = [];
+    /**
+     * Основной обработчик перечня case условий
+     * @param array $case перечень case условий
+     * @param suql\core\SuQLField $ofield объект поля к которому применяется модификатор
+     * @param array $params не используется в данном модификаторе
+     */
+    public static function mod_case($case, $ofield, $params)
+    {
+        $fieldName = $ofield->getField();
+        $caseList = [];
 
-    foreach ($case as $when => $then) {
-      if ($when === 'default') {
-        $caseList[] = (new PlaceholderHelper("else ?"))->bind($then);
-      } else {
-        $caseList[] = "when " . str_replace('$', $fieldName, $when) . (new PlaceholderHelper(" then ?"))->bind($then);
-      }
+        foreach ($case as $when => $then) {
+            if ($when === 'default') {
+                $caseList[] = (new PlaceholderHelper("else ?"))->bind($then);
+            } else {
+                $caseList[] = "when " . str_replace('$', $fieldName, $when) . (new PlaceholderHelper(" then ?"))->bind($then);
+            }
+        }
+
+        $ofield->setField('case ' . implode(' ', $caseList) . ' end');
     }
-
-    $ofield->setField('case ' . implode(' ', $caseList) . ' end');
-  }
-
-  public static function mod_role($ofield, $params) {
-    self::mod_case([
-      '$ = 1'   => 'admin',
-      '$ = 2'   => 'user',
-      '$ = 3'   => 'guest',
-      'default' => '',
-    ], $ofield, $params);
-  }
-
-  public static function mod_even($ofield, $params) {
-    self::mod_case([
-      '$ = 1' => 'no',
-      '$ = 0' => 'yes',
-    ], $ofield, $params);
-  }
 }
