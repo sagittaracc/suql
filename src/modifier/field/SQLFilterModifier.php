ea05ec2fb8610c6ee1c98f534a1cd64df1e90cff
arutyunyan <arutyunyan@bolid.ru>
2021-04-29 16:36:30 +0300

    add todo

--- src/modifier/field/SQLFilterModifier.php
+++ src/modifier/field/SQLFilterModifier.php
@@ -2,20 +2,19 @@
 
 namespace suql\modifier\field;
 
+// TODO: Должно наследоваться от SQLWhereModifier
 class SQLFilterModifier
 {
-  public static function mod_filter($ofield, $params)
-  {
-    $filterFunction = $params[0];
-    $filterValue = $params[1];
-
-    if (!is_null($filterValue))
+    public static function mod_filter($ofield, $params)
     {
-      $whereModifier = "mod_$filterFunction";
-      if (method_exists(SQLWhereModifier::class, $whereModifier))
-      {
-        SQLWhereModifier::$whereModifier($ofield, [$filterValue], true);
-      }
+        $filterFunction = $params[0];
+        $filterValue = $params[1];
+
+        if (!is_null($filterValue)) {
+            $whereModifier = "mod_$filterFunction";
+            if (method_exists(SQLWhereModifier::class, $whereModifier)) {
+                SQLWhereModifier::$whereModifier($ofield, [$filterValue], true);
+            }
+        }
     }
-  }
 }
