2e382baf37b85970920d5703623ca4bfa25921f6
arutyunyan <arutyunyan@bolid.ru>
2021-04-30 10:51:24 +0300

    working on phpdocs

--- src/core/SuQLInsert.php
+++ src/core/SuQLInsert.php
@@ -5,38 +5,69 @@ namespace suql\core;
 use sagittaracc\PlaceholderHelper;
 use suql\core\InsertQueryInterface;
 
+/**
+ * Обработчик insert запросов
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
 class SuQLInsert extends SuQLQuery implements InsertQueryInterface
 {
-  private $table = null;
-  private $values = [];
-
-  public function addInto($table)
-  {
-    $this->table = $table;
-  }
-
-  public function addValue($field, $value)
-  {
-    $this->values[$field] = (new PlaceholderHelper("?"))->bind($value);
-  }
-
-  public function addPlaceholder($field, $placeholder)
-  {
-    $this->values[$field] = $placeholder;
-  }
-
-  public function getTable()
-  {
-    return $this->table;
-  }
-
-  public function getFields()
-  {
-    return implode(',', array_keys($this->values));
-  }
-
-  public function getValues()
-  {
-    return implode(',', array_values($this->values));
-  }
+    /**
+     * @var string таблица в которую осуществляется добавление записей
+     */
+    private $table = null;
+    /**
+     * @var array массив записей для добавления
+     */
+    private $values = [];
+    /**
+     * Задать таблицу в которую будут добавляться записи
+     * @param string $table название таблицы
+     */
+    public function addInto($table)
+    {
+        $this->table = $table;
+    }
+    /**
+     * Добавить значение поля для добавления
+     * @param string $field название поля
+     * @param string $value значение поля
+     */
+    public function addValue($field, $value)
+    {
+        $this->values[$field] = (new PlaceholderHelper("?"))->bind($value);
+    }
+    /**
+     * Задать добавление значения по плейсхолдеру
+     * @param string $field название поля
+     * @param string $placeholder название плейсхолдера
+     */
+    public function addPlaceholder($field, $placeholder)
+    {
+        $this->values[$field] = $placeholder;
+    }
+    /**
+     * Получить таблицу в которую добавляются записи
+     * @return string
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+    /**
+     * Получить строку с перечнем полей для добавления
+     * @return string
+     */
+    public function getFields()
+    {
+        return implode(',', array_keys($this->values));
+    }
+    /**
+     * Получить строку с перечнем значений для добавления
+     * @return string
+     */
+    public function getValues()
+    {
+        return implode(',', array_values($this->values));
+    }
 }
