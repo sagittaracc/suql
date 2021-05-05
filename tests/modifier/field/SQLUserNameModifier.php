1b8c72c8bb551c7fd146751bfdf4df20ee9cf8f1
arutyunyan <arutyunyan@bolid.ru>
2021-05-05 14:29:13 +0300

    Start develop

--- tests/modifier/field/SQLUserNameModifier.php
+++
@@ -1,8 +0,0 @@
-<?php
-class SQLUserNameModifier
-{
-  public static function mod_ucname($ofield, $params)
-  {
-    $ofield->setField("CONCAT(UCASE(LEFT({$ofield->getField()}, 1)), SUBSTRING({$ofield->getField()}, 2))");
-  }
-}
