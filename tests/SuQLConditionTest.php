95db5bafddb2421b9913b030238013ef5ceb8127
arutyunyan <arutyunyan@bolid.ru>
2021-05-06 15:48:16 +0300

    add some tests

--- tests/SuQLConditionTest.php
+++ tests/SuQLConditionTest.php
@@ -6,38 +6,48 @@ use PHPUnit\Framework\TestCase;
 use suql\core\SuQLBetweenParam;
 use suql\core\SuQLCondition;
 use suql\core\SuQLFieldName;
+use suql\core\SuQLInParam;
+use suql\core\SuQLLikeParam;
 use suql\core\SuQLSimpleParam;
 
 final class SuQLConditionTest extends TestCase
 {
-    private $simple_param;
-    private $between_param;
+    private $fieldUserId;
+    private $fieldUserName;
+
+    private $simpleParam;
+    private $betweenParam;
+    private $inParam;
+    private $likeParam;
 
     protected function setUp(): void
     {
-        $this->simple_param = new SuQLSimpleParam(
-            new SuQLFieldName('users', 'id'),
-            [0]
-        );
+        $this->fieldUserId = new SuQLFieldName('users', 'id');
+        $this->fieldUserName = new SuQLFieldName('users', 'name');
 
-        $this->between_param = new SuQLBetweenParam(
-            new SuQLFieldName('users', 'id'),
-            [1, 3]
-        );
+        $this->simpleParam = new SuQLSimpleParam($this->fieldUserId, [1]);
+        $this->betweenParam = new SuQLBetweenParam($this->fieldUserId, [1, 3]);
+        $this->inParam = new SuQLInParam($this->fieldUserId, [1, 2, 3]);
+        $this->likeParam = new SuQLLikeParam($this->fieldUserName, ['sagittaracc']);
     }
 
     protected function tearDown(): void
     {
-        $this->simple_param = null;
-        $this->between_param = null;
+        $this->fieldUserId = null;
+        $this->fieldUserName = null;
+
+        $this->simpleParam = null;
+        $this->betweenParam = null;
+        $this->inParam = null;
+        $this->likeParam = null;
     }
 
     public function testSimpleParamCondition(): void
     {
-        $expectedCondition = 'id % 2 = :ph0_fc02896e3034a4ed53259916e2e2d82d';
-        $expectedParams = [':ph0_fc02896e3034a4ed53259916e2e2d82d' => 0];
+        $expectedCondition = 'id = :ph0_fc02896e3034a4ed53259916e2e2d82d';
+        $expectedParams = [':ph0_fc02896e3034a4ed53259916e2e2d82d' => 1];
 
-        $actualCondition = new SuQLCondition($this->simple_param, '$ % 2 = ?');
+        $actualCondition = new SuQLCondition($this->simpleParam, '$ = ?');
 
         $this->assertEquals($expectedCondition, $actualCondition->getCondition());
         $this->assertEquals($expectedParams, $actualCondition->getParams());
@@ -51,8 +61,36 @@ final class SuQLConditionTest extends TestCase
             ':ph1_fc02896e3034a4ed53259916e2e2d82d' => 3,
         ];
 
-        $actualCondition = new SuQLCondition($this->between_param, '$ between ?');
+        $actualCondition = new SuQLCondition($this->betweenParam, '$ between ?');
         $actualCondition->setFormat('%t.%n');
+
+        $this->assertEquals($expectedCondition, $actualCondition->getCondition());
+        $this->assertEquals($expectedParams, $actualCondition->getParams());
+    }
+
+    public function testInParamCondition(): void
+    {
+        $expectedCondition = 'id in (:ph0_fc02896e3034a4ed53259916e2e2d82d,:ph1_fc02896e3034a4ed53259916e2e2d82d,:ph2_fc02896e3034a4ed53259916e2e2d82d)';
+        $expectedParams = [
+            ':ph0_fc02896e3034a4ed53259916e2e2d82d' => 1,
+            ':ph1_fc02896e3034a4ed53259916e2e2d82d' => 2,
+            ':ph2_fc02896e3034a4ed53259916e2e2d82d' => 3,
+        ];
+
+        $actualCondition = new SuQLCondition($this->inParam, '$ in ?');
+
+        $this->assertEquals($expectedCondition, $actualCondition->getCondition());
+        $this->assertEquals($expectedParams, $actualCondition->getParams());
+    }
+
+    public function testLikeParamCondition(): void
+    {
+        $expectedCondition = 'users.name like :ph0_12cb8fae9701df6e8e8b1b972362a7ff';
+        $expectedParams = [
+            ':ph0_12cb8fae9701df6e8e8b1b972362a7ff' => '%sagittaracc%',
+        ];
+
+        $actualCondition = new SuQLCondition($this->likeParam, '$ like ?', '%t.%n');
 
         $this->assertEquals($expectedCondition, $actualCondition->getCondition());
         $this->assertEquals($expectedParams, $actualCondition->getParams());
