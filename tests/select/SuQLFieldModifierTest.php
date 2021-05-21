d20491d5e38a9c5a5f17175aa37c915dc23cbea9
arutyunyan <arutyunyan@bolid.ru>
2021-05-21 17:24:57 +0300

    done with test refactoring

--- tests/select/SuQLFieldModifierTest.php
+++ tests/select/SuQLFieldModifierTest.php
@@ -2,37 +2,10 @@
 
 declare(strict_types=1);
 
-use PHPUnit\Framework\TestCase;
 use sagittaracc\StringHelper;
-use suql\core\SuQLObject;
-use suql\core\SuQLScheme;
-use suql\builder\SQLDriver;
-use suql\core\SuQLCondition;
-use suql\core\SuQLExpression;
-use suql\core\SuQLFieldName;
-use suql\core\SuQLPlaceholder;
-use suql\core\SuQLSimpleParam;
 
-final class SuQLObjectTest extends TestCase
+final class SuQLFieldModifierTest extends SuQLMock
 {
-    private $osuql;
-
-    protected function setUp(): void
-    {
-        $scheme = new SuQLScheme();
-        $scheme->rel('users', 'user_group', 'users.id = user_group.user_id');
-        $scheme->rel('user_group', 'groups', 'user_group.group_id = groups.id');
-
-        $driver = new SQLDriver('mysql');
-
-        $this->osuql = new SuQLObject($scheme, $driver);
-    }
-
-    protected function tearDown(): void
-    {
-        $this->osuql = null;
-    }
-
     public function testCallbackModifier(): void
     {
         $sql = StringHelper::trimSql(<<<SQL
@@ -40,7 +13,7 @@ final class SuQLObjectTest extends TestCase
                 users.id
             from users
             where users.id > 3
-SQL);
+        SQL);
 
         $this->osuql->addSelect('callback_modifier');
         $this->osuql->getQuery('callback_modifier')->addFrom('users');
