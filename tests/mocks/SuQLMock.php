601cb617553adfa15d089354501fd21ce54fb5aa
arutyunyan <arutyunyan@bolid.ru>
2021-05-21 17:05:10 +0300

    refactor

--- tests/mocks/SuQLMock.php
+++ tests/mocks/SuQLMock.php
@@ -14,6 +14,9 @@ class SuQLMock extends TestCase
     protected function setUp(): void
     {
         $scheme = new SuQLScheme();
+        $scheme->rel('users', 'user_group', 'users.id = user_group.user_id');
+        $scheme->rel('user_group', 'groups', 'user_group.group_id = groups.id');
+
         $driver = new SQLDriver('mysql');
 
         $this->osuql = new SuQLObject($scheme, $driver);
