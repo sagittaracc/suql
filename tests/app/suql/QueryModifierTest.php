54684781d476f4f9d7f8156ee32e1843a12c3790
arutyunyan <arutyunyan@bolid.ru>
2021-05-25 16:16:18 +0300

    add query distinct test

---
+++ tests/app/suql/QueryModifierTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use app\models\User;
+use PHPUnit\Framework\TestCase;
+use sagittaracc\StringHelper;
+
+final class QueryModifierTest extends TestCase
+{
+    /**
+     * SELECT DISTINCT ... FROM <table>
+     */
+    public function testSelectDistinct(): void
+    {
+        $sql = StringHelper::trimSql(<<<SQL
+            select distinct
+                users.name
+            from users
+SQL);
+
+        $query = User::distinct()->select(['name']);
+
+        $this->assertEquals($sql, $query->getRawSql());
+    }
+}
