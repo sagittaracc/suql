9c4031e072afd7ecc3f8fcf88e288d953995abd8
arutyunyan <arutyunyan@bolid.ru>
2021-05-27 17:20:37 +0300

    add db tests

--- tests/resurs/records/ResursRecord.php
+++ tests/resurs/records/ResursRecord.php
@@ -23,6 +23,6 @@ abstract class ResursRecord extends SuQL
 
     public function getDb()
     {
-        return null;
+        return new PDO('mysql:host=localhost;dbname=resurs', 'root', '');
     }
 }
