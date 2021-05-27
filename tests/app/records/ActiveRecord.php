a14cc3a46999101ee5fdfd0200413e118db95fef
yriiarutiunian <yriiarutiunian@gmail.com>
2021-05-28 00:00:01 +0300

    refactor

--- tests/app/records/ActiveRecord.php
+++ tests/app/records/ActiveRecord.php
@@ -3,11 +3,22 @@
 namespace app\records;
 
 use suql\syntax\SuQL;
+use resurs\modifiers\CustomModifier;
 
 abstract class ActiveRecord extends SuQL
 {
     protected static $schemeClass = 'app\\schema\\AppScheme';
     protected static $sqlDriver = 'mysql';
+
+    protected function modifierList()
+    {
+        return array_merge(
+            parent::modifierList(),
+            [
+                CustomModifier::class,
+            ]
+        );
+    }
 
     public function getDb()
     {
