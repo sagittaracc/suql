c6356e97705c7fbd6bcdf7cf52748219351d1ad9
yriiarutiunian <yriiarutiunian@gmail.com>
2021-05-27 23:56:26 +0300

    refactor folder app structure

---
+++ tests/app/modifiers/CustomModifier.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace resurs\modifiers;
+
+use suql\modifier\field\SQLCaseModifier;
+
+class CustomModifier extends SQLCaseModifier
+{
+    public static function mod_custom_one($ofield, $params)
+    {
+        // ... Code here
+    }
+}
