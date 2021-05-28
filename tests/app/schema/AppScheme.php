b3cca3ca8cc4564b37ed114c19481ba42b19f6d1
arutyunyan <arutyunyan@bolid.ru>
2021-05-28 10:32:14 +0300

    refactor

--- tests/app/schema/AppScheme.php
+++ tests/app/schema/AppScheme.php
@@ -8,9 +8,11 @@ class AppScheme extends Scheme
 {
     function __construct()
     {
+        // Связь между реальными таблицами в базе данных
         $this->rel('users', 'user_group', 'users.id = user_group.user_id');
         $this->rel('user_group', 'groups', 'user_group.group_id = groups.id');
 
+        // Связь с абстрактной вьюхой
         $this->rel('users', 'last_registration', 'users.registration = last_registration.lastRegistration');
     }
 }
