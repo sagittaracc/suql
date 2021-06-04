a87b729bf1e4db2a1bc725fe8346bb3a1f11bd9e
arutyunyan <arutyunyan@bolid.ru>
2021-06-04 17:08:53 +0300

    Fix #281. Smart join with a view

--- tests/select/SuQLSmartJoinTest.php
+++ tests/select/SuQLSmartJoinTest.php
@@ -38,4 +38,44 @@ SQL);
         $this->assertEquals($sql, $suql);
         $this->assertNull($this->osuql->getSQL(['smart_join']));
     }
+
+    public function testSmartJoinWithView(): void
+    {
+        /**
+         * SELECT
+         *   ...
+         * FROM <table>
+         * [INNER|LEFT|RIGHT] JOIN <join-table-1> ON <join-1>
+         * [INNER|LEFT|RIGHT] JOIN <join-table-2> ON <join-2>
+         * [INNER|LEFT|RIGHT] JOIN (...)
+         */
+        $sql = StringHelper::trimSql(<<<SQL
+            select
+                users.id
+            from users
+            inner join user_group on users.id = user_group.user_id
+            inner join (
+                select
+                    *
+                from groups
+                limit 1
+            ) first_group on user_group.group_id = first_group.id
+SQL);
+
+        $this->osuql->getScheme()->rel('user_group', 'first_group', 'user_group.group_id = first_group.id');
+
+        $this->osuql->addSelect('smart_join');
+        $this->osuql->getQuery('smart_join')->addFrom('users');
+        $this->osuql->getQuery('smart_join')->addField('users', 'id');
+        $this->osuql->getQuery('smart_join')->addSmartJoin('users', 'first_group');
+
+        $this->osuql->addSelect('first_group');
+        $this->osuql->getQuery('first_group')->addFrom('groups');
+        $this->osuql->getQuery('first_group')->addLimit(1);
+
+        $suql = $this->osuql->getSQL(['smart_join']);
+
+        $this->assertEquals($sql, $suql);
+        $this->assertNull($this->osuql->getSQL(['smart_join']));
+    }
 }
