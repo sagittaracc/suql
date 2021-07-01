32c30b417c83727e450be26be853417adf26147c
arutyunyan <arutyunyan@bolid.ru>
2021-07-01 09:29:12 +0300

    refactor

--- tests/suql/QueryTest.php
+++ tests/suql/QueryTest.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use sagittaracc\StringHelper;
 use suql\db\Container;
 use suql\syntax\Query;
 use test\suql\models\TableName;
@@ -11,35 +10,24 @@ use test\suql\models\TempTable;
 
 final class QueryTest extends TestCase
 {
-    public function testQuery(): void
+    public function setUp(): void
     {
+        // Create a database
         Container::create(require('config/db.php'));
         Query::create('connection', 'create database db_test')->exec();
         Container::add(require('config/db-test.php'));
         Query::create('db_test', 'create table table_name(field int)')->exec();
         Query::create('db_test', 'insert into table_name (field) values (1), (2), (3)')->exec();
-
-        $this->testTempTable();
-
-        $data = TableName::all()->fetchAll();
-        $firstRow = TableName::all()->order(['field' => 'desc'])->fetchOne();
-        $count = Query::create('db_test', 'delete from table_name')->exec();
-
-        Query::create('db_test', 'drop table table_name')->exec();
-        Query::create('db_test', 'drop database db_test')->exec();
-
-        $this->assertEquals(3, $count);
-        $this->assertEquals([
-            ['field' => '1'],
-            ['field' => '2'],
-            ['field' => '3'],
-        ], $data);
-        $this->assertEquals([
-            'field' => 3,
-        ], $firstRow);
     }
 
-    private function testTempTable()
+    public function tearDown(): void
+    {
+        // Drop the database
+        Query::create('db_test', 'drop table table_name')->exec();
+        Query::create('db_test', 'drop database db_test')->exec();
+    }
+
+    public function testQuery(): void
     {
         $tableData = [
             ['id' => 1, 'name' => 'mario'],
@@ -54,5 +42,19 @@ final class QueryTest extends TestCase
             ['id' => '2', 'name' => 'fayword', 'field' => '2'],
             ['id' => '3', 'name' => '1nterFucker', 'field' => '3'],
         ], $query->fetchAll());
+
+        $data = TableName::all()->fetchAll();
+        $firstRow = TableName::all()->order(['field' => 'desc'])->fetchOne();
+        $count = Query::create('db_test', 'delete from table_name')->exec();
+
+        $this->assertEquals(3, $count);
+        $this->assertEquals([
+            ['field' => '1'],
+            ['field' => '2'],
+            ['field' => '3'],
+        ], $data);
+        $this->assertEquals([
+            'field' => 3,
+        ], $firstRow);
     }
 }
