7b5c3e5212dbc81a1118862963c7fe1fff619481
arutyunyan <arutyunyan@bolid.ru>
2021-07-22 18:33:36 +0300

    парсинг smart date

--- src/syntax/SmartDate.php
+++ src/syntax/SmartDate.php
@@ -3,34 +3,25 @@
 class SmartDate
 {
     private $smartDate;
+    private $type;
 
     protected $patternList = [
         'simple' => [
-            '/yesterday/' => "DATE_FORMAT($, '%Y-%m-%d') = DATE_FORMAT(NOW() - INTERVAL 1 DAY, '%Y-%m-%d')",
-            '/today/'     => "DATE_FORMAT($, '%Y-%m-%d') = DATE_FORMAT(NOW(), '%Y-%m-%d')",
-            '/tomorrow/'  => "DATE_FORMAT($, '%Y-%m-%d') = DATE_FORMAT(NOW() + INTERVAL 1 DAY, '%Y-%m-%d')",
+            '/yesterday/' => true,
+            '/today/'     => true,
+            '/tomorrow/'  => true,
         ],
         'ago' => [
-            '/(\d+) days? ago/'  => "DATE_FORMAT($, '%Y-%m-%d') = DATE_FORMAT(NOW() - INTERVAL # DAY, '%Y-%m-%d')",
-            '/(\d+) weeks? ago/' => "DATE_FORMAT($, '%Y-%m-%d') = DATE_FORMAT(NOW() - INTERVAL # WEEK, '%Y-%m-%d')",
-            '/(\d+) years? ago/' => "DATE_FORMAT($, '%Y-%m-%d') = DATE_FORMAT(NOW() - INTERVAL # YEAR, '%Y-%m-%d')",
+            '/(\d+) days? ago/'  => true,
+            '/(\d+) weeks? ago/' => true,
+            '/(\d+) years? ago/' => true,
         ],
         'last' => [
-            '/last (\d+) days?/'  => "DATE_FORMAT($, '%Y-%m-%d') >= DATE_FORMAT(NOW() - INTERVAL # DAY, '%Y-%m-%d') AND DATE_FORMAT($, '%Y-%m-%d') <= DATE_FORMAT(NOW(), '%Y-%m-%d')",
-            '/last (\d+) weeks?/' => "DATE_FORMAT($, '%Y-%m-%d') >= DATE_FORMAT(NOW() - INTERVAL # WEEK, '%Y-%m-%d') AND DATE_FORMAT($, '%Y-%m-%d') <= DATE_FORMAT(NOW(), '%Y-%m-%d')",
-            '/last (\d+) years?/' => "DATE_FORMAT($, '%Y-%m-%d') >= DATE_FORMAT(NOW() - INTERVAL # YEAR, '%Y-%m-%d') AND DATE_FORMAT($, '%Y-%m-%d') <= DATE_FORMAT(NOW(), '%Y-%m-%d')",
+            '/last (\d+) days?/'  => true,
+            '/last (\d+) weeks?/' => true,
+            '/last (\d+) years?/' => true,
         ],
     ];
-
-    function __construct($smartDate)
-    {
-        $this->smartDate = $smartDate;
-    }
-
-    public static function create($smartDate)
-    {
-        return new static($smartDate);
-    }
 
     protected function getSmartDateType()
     {
@@ -42,20 +33,22 @@ class SmartDate
             return 'simple';
     }
 
-    public function getSql()
+    function __construct($smartDate)
     {
-        $cases = $this->patternList[$this->getSmartDateType()];
+        $this->smartDate = $smartDate;
+
+        $this->type = $this->getSmartDateType();
+        $cases = $this->patternList[$this->type];
 
         foreach ($cases as $case => $sql) {
             if (preg_match($case, $this->smartDate, $matches)) {
-                if (!empty($matches)) {
-                    $sql = str_replace('#', $matches[1], $sql);
-                }
-
-                return $sql;
+                // $matches[1];
             }
         }
+    }
 
-        return false;
+    public static function create($smartDate)
+    {
+        return new static($smartDate);
     }
 }
