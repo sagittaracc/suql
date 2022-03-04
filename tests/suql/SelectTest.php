6bdff79737016e85306b0dd70d764ea32a9c0110
arutyunyan <arutyunyan@bolid.ru>
2022-03-04 10:59:42 +0300

    update tests

--- tests/suql/SelectTest.php
+++ tests/suql/SelectTest.php
@@ -9,27 +9,54 @@ use test\suql\models\Query4;
 
 final class SelectTest extends TestCase
 {
+    /**
+     * Example:
+     * 
+     * select * from table
+     * 
+     */
     public function testSelectAll(): void
     {
         $expected = StringHelper::trimSql(require('queries/q1.php'));
         $actual = Query1::all()->getRawSql();
         $this->assertEquals($expected, $actual);
     }
-
+    /**
+     * Example:
+     * 
+     * select table.* from table
+     * 
+     */
     public function testSelectAllWithTableName(): void
     {
         $expected = StringHelper::trimSql(require('queries/q2.php'));
         $actual = Query1::all()->select(['*'])->getRawSql();
         $this->assertEquals($expected, $actual);
     }
-
+    /**
+     * Example:
+     * 
+     * select
+     *     table.f1,
+     *     table.f2
+     * from table
+     * 
+     */
     public function testSelectFieldList(): void
     {
         $expected = StringHelper::trimSql(require('queries/q3.php'));
         $actual = Query1::all()->select(['f1', 'f2'])->getRawSql();
         $this->assertEquals($expected, $actual);
     }
-
+    /**
+     * Example:
+     * 
+     * select
+     *     table.f1 as af1,
+     *     table.f2 as af2
+     * from table
+     * 
+     */
     public function testSelectUsingAliases(): void
     {
         $expected = StringHelper::trimSql(require('queries/q4.php'));
@@ -39,7 +66,16 @@ final class SelectTest extends TestCase
         ])->getRawSql();
         $this->assertEquals($expected, $actual);
     }
-
+    /**
+     * Example:
+     * 
+     * select
+     *     t.f1,
+     *     t.f2,
+     *     t.f3
+     * from table t
+     * 
+     */
     public function testSelectWithTableAlias(): void
     {
         $expected = StringHelper::trimSql(require('queries/q5.php'));
