ef02c9ec6ef6cc61bea59dad905cd816f0634b41
arutyunyan <arutyunyan@bolid.ru>
2022-03-04 14:01:19 +0300

    tests refactor

--- tests/suql/NestedQueryTest.php
+++ tests/suql/NestedQueryTest.php
@@ -8,6 +8,20 @@ use test\suql\models\Query6;
 
 final class NestedQueryTest extends TestCase
 {
+    /**
+     * Example:
+     * 
+     * select
+     *     t.f1,
+     *     t.f2
+     * from (
+     *     select
+     *         table.f1,
+     *         table.f2,
+     *         table.f3
+     *     from table
+     * ) t
+     */
     public function testNestedQuery(): void
     {
         $expected = StringHelper::trimSql(require('queries/q14.php'));
--- tests/suql/models/Query6.php
+++ tests/suql/models/Query6.php
@@ -8,14 +8,14 @@ class Query6 extends ActiveRecord
 {
     public function table()
     {
-        return Query7::all()->select(['field_1', 'field_2', 'field_3']);
+        return Query7::all()->select(['f1', 'f2', 'f3']);
     }
 
     public function fields()
     {
         return [
-            'field_1',
-            'field_2',
+            'f1',
+            'f2',
         ];
     }
 }
