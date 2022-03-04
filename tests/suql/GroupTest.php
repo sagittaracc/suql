4e486216e673de921650a38a8df3198aaa64913c
arutyunyan <arutyunyan@bolid.ru>
2022-03-04 09:48:50 +0300

    tests refactor

--- tests/suql/GroupTest.php
+++ tests/suql/GroupTest.php
@@ -2,37 +2,30 @@
 
 declare(strict_types=1);
 
-use test\suql\models\ActiveGroups;
 use PHPUnit\Framework\TestCase;
 use sagittaracc\StringHelper;
+use test\suql\models\Query1;
 
 final class GroupTest extends TestCase
 {
-    /**
-     * SELECT
-     *   ...
-     * FROM <table>
-     * ...
-     * GROUP BY
-     *   <table-1>.<field-1>,
-     *   <table-1>.<field-2>,
-     *   ...,
-     *   <table-2>.<field-1>,
-     *   ...
-     */
     public function testSelectGroup(): void
     {
         $sql = StringHelper::trimSql(<<<SQL
             select
-                groups.name,
-                count(groups.name) as count
-            from users
-            inner join user_group on users.id = user_group.user_id
-            inner join groups on user_group.group_id = groups.id
-            group by groups.name
+                table_3.f1,
+                count(table_3.f1) as count
+            from table_1
+            inner join table_2 on table_1.id = table_2.id
+            inner join table_3 on table_2.id = table_3.id
+            group by table_3.f1
 SQL);
 
-        $query = ActiveGroups::all();
+        $query = Query1::all()
+            ->getQuery2()
+            ->getQuery3()
+                ->select(['f1'])
+            ->group('f1')
+            ->count(['f1' => 'count']);
         
         $this->assertEquals($sql, $query->getRawSql());
     }
