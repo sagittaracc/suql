38848d76fd1184c28697b09531bc33a05b1b9222
arutyunyan <arutyunyan@bolid.ru>
2022-03-09 15:19:16 +0300

    tests refactor

--- tests/suql/RawViewTest.php
+++ tests/suql/RawViewTest.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use suql\db\Container;
 use suql\syntax\Query;
-use test\suql\models\Query10;
+use test\suql\models\Query17;
 use test\suql\schema\AppScheme;
 
-final class FetchTest extends TestCase
+final class RawViewTest extends TestCase
 {
     public function setUp(): void
     {
@@ -25,39 +25,16 @@ final class FetchTest extends TestCase
     {
         // Drop the database
         Query::create('drop table table_10')->setConnection('db_test')->exec();
+        Query::create('drop view if exists view_17')->setConnection('db_test')->exec();
         Query::create('drop database db_test')->setConnection('db_test')->exec();
     }
 
-    public function testFetchAll(): void
+    public function testView(): void
     {
-        $data = Query10::all()->select(['f1'])->fetchAll();
-
+        $data = Query17::all()->fetchAll();
         $this->assertEquals([
-            ['f1' => '1'],
             ['f1' => '2'],
-            ['f1' => '3'],
             ['f1' => '4'],
-            ['f1' => '5'],
-            ['f1' => '6'],
-        ], $data);
-    }
-
-    public function testFetchOne(): void
-    {
-        $data = Query10::all()->select(['f1'])->order(['f1' => 'desc'])->fetchOne();
-        $this->assertEquals(['f1' => 6], $data);
-    }
-
-    public function testFetchWithDbManager(): void
-    {
-        $db = new suql\db\Manager('db_test', AppScheme::class);
-        $data = $db->entity('table_10')->select(['f1'])->fetchAll();
-        $this->assertEquals([
-            ['f1' => '1'],
-            ['f1' => '2'],
-            ['f1' => '3'],
-            ['f1' => '4'],
-            ['f1' => '5'],
             ['f1' => '6'],
         ], $data);
     }
