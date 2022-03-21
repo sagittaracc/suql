92d7c3501e85405e1fc3d92cfa016ee27d3d0bdd
arutyunyan <arutyunyan@bolid.ru>
2022-03-21 17:41:42 +0300

    update docs

--- src/manager/EntityManager.php
+++ src/manager/EntityManager.php
@@ -11,20 +11,35 @@ use suql\db\Entity;
  */
 class EntityManager
 {
+    /**
+     * @var string используемая схема
+     */
     private $schemeClass;
-
+    /**
+     * @var string используемый билдер
+     */
     private $builderClass;
-
+    /**
+     * Задать схему
+     * @param string $schemeClass
+     */
     public function setScheme($schemeClass)
     {
         $this->schemeClass = $schemeClass;
     }
-
+    /**
+     * Задать билдер
+     * @param string $builderClass
+     */
     public function setBuilder($builderClass)
     {
         $this->builderClass = $builderClass;
     }
-
+    /**
+     * Получить объект из репозитория
+     * @param string имя класса объекта или таблицы в базе данных
+     * @return suql\syntax\SuQL
+     */
     public function getRepository($repositoryQuery)
     {
         $repository = null;
