c1c2618797213ba45dafa499a799f485b6d04a15
arutyunyan <arutyunyan@bolid.ru>
2022-05-18 14:19:11 +0300

    fix tests

--- tests/suql/queries/mysql/q36.php
+++ tests/suql/queries/mysql/q36.php
@@ -3,7 +3,7 @@ return <<<SQL
     select
         `table_1`.`f1` as `af1`,
         `table_3`.`f2` as `af2`,
-        count(`table_3`.`f3`)
+        count(`table_3`.`f3`) as `af3`
     from `table_1`
     inner join `table_2` on `table_1`.`id` = `table_2`.`id`
     inner join `table_3` on `table_2`.`id` = `table_3`.`id`
