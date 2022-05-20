97f9acbe98bcfaf604182d03a9d44211742c1fea
arutyunyan <arutyunyan@bolid.ru>
2022-05-20 11:38:50 +0300

    Не до конца выпилил контроллеры

--- src/syntax/entity/SuQLTable.php
+++ src/syntax/entity/SuQLTable.php
@@ -4,12 +4,14 @@ namespace suql\syntax\entity;
 
 use ReflectionMethod;
 use suql\annotation\TableAnnotation;
+use suql\syntax\DbObject;
 use suql\syntax\Model;
+use suql\syntax\QueryObject;
 use suql\syntax\SuQL;
 use suql\syntax\TableInterface;
 use test\suql\modifiers\CustomModifier;
 
-abstract class SuQLTable extends SuQL implements TableInterface
+abstract class SuQLTable extends SuQL implements TableInterface, DbObject, QueryObject
 {
     use Model;
     /**
