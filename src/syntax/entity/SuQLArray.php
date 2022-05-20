7db4deb48ad574b8d7a06302b98b3943883c5fe9
arutyunyan <arutyunyan@bolid.ru>
2022-05-20 11:43:54 +0300

    Начало работы над SuQL Array

---
+++ src/syntax/entity/SuQLArray.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace suql\syntax\entity;
+
+use suql\syntax\ArrayInterface;
+use suql\syntax\SuQL;
+
+abstract class SuQLArray extends SuQL implements ArrayInterface
+{
+}
---
+++ src/syntax/interface/ArrayInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace suql\syntax;
+
+/**
+ * Объект обращающийся к массиву
+ * 
+ * @author sagittaracc <sagittaracc@gmail.com>
+ */
+interface ArrayInterface
+{
+    public function data();
+}
