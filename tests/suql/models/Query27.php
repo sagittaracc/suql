0202dd2cdf11c82b6aa7812049fe40c1cd94dbe8
arutyunyan <arutyunyan@bolid.ru>
2022-06-08 14:19:43 +0300

    refactor

--- tests/suql/models/Query27.php
+++ tests/suql/models/Query27.php
@@ -7,11 +7,6 @@ use suql\syntax\entity\SuQLService;
 
 class Query27 extends SuQLService
 {
-    public function query()
-    {
-        return 'temp_query';
-    }
-
     public function getDb()
     {
         return Container::get('db_test');
