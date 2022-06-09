7534373111766ae792a5c61cb9d4ccf10e6b8cfd
arutyunyan <arutyunyan@bolid.ru>
2022-06-09 13:52:49 +0300

    refactor

--- tests/suql/models/Query18.php
+++ tests/suql/models/Query18.php
@@ -5,12 +5,9 @@ namespace test\suql\models;
 use suql\builder\MySQLBuilder;
 use test\suql\models\buffers\Buffer;
 use test\suql\models\tables\TestSqliteTable;
-use test\suql\schema\AppScheme;
 
 # [Table(name="table_1")]
 class Query18 extends TestSqliteTable
 {
     protected static $bufferClass = Buffer::class;
-    protected static $schemeClass = AppScheme::class;
-    protected static $builderClass = MySQLBuilder::class;
 }
