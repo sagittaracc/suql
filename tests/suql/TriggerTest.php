89c9a901a006e9bec1e32aab4211ccd2907645a3
arutyunyan <arutyunyan@bolid.ru>
2022-07-22 17:51:50 +0300

    tests refactor

--- tests/suql/TriggerTest.php
+++ tests/suql/TriggerTest.php
@@ -15,15 +15,11 @@ final class TriggerTest extends TestCase
         Container::create(require('config/db-null.php'));
         Query::create('create database db_test')->setConnection('connection')->exec();
         Container::add(require('config/db.php'));
-        Query::create('create table table_10(f1 int, f2 int, primary key (f1))')->setConnection('db_test')->exec();
-        Query::create('insert into table_10 (f1, f2) values (1, 1), (2, 2), (3, 3)')->setConnection('db_test')->exec();
-        Query::create('insert into table_10 (f1, f2) values (?, ?), (?, ?), (?, ?)')->setConnection('db_test')->exec([4, 4, 5, 5, 6, 6]);
     }
 
     public function tearDown(): void
     {
         // Drop the database
-        Query::create('drop table table_10')->setConnection('db_test')->exec();
         Query::create('drop database db_test')->setConnection('db_test')->exec();
     }
 
