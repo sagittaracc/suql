132a0ef0806c2ccfe963a9fe8026a6dc61bff6ca
arutyunyan <arutyunyan@bolid.ru>
2022-07-25 16:20:07 +0300

    add suql.js

---
+++ src/js/suql.js
@@ -0,0 +1,15 @@
+function assign(a, b)
+{
+    a.value = typeof b === "function" ? b(a.value) : b
+    
+    var elementId = a.path
+    if (typeof a.value === "array") {
+        // ...
+    }
+    else if (typeof a.value === "object") {
+        // ...
+    }
+    else {
+        document.getElementById(elementId).text = a.value
+    }
+}
