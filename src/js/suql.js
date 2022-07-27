293ad4c190529489f31d5a47507c679edf9537ac
arutyunyan <arutyunyan@bolid.ru>
2022-07-27 13:40:24 +0300

    доработка sg-repeat

--- src/js/suql.js
+++ src/js/suql.js
@@ -3,11 +3,16 @@ function assign(a, b)
     a.value = typeof b === "function" ? b(a.value) : b
     
     var elementId = a.path
-    if (typeof a.value === "array") {
-        // ...
-    }
-    else if (typeof a.value === "object") {
-        // ...
+    if (typeof a.value === "object") {
+        var content = "";
+        for(var i = 0, n = a.value.length; i < n; i++) {
+            var template = a.template;
+            for (variable in a.value[i]) {
+                template = template.replace("{{" + variable + "}}", a.value[i][variable])
+            }
+            content += template
+        }
+        document.getElementById(elementId).innerHTML = content;
     }
     else {
         document.getElementById(elementId).textContent = a.value
