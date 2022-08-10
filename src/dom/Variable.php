3ee6ec140218839db79fdce8c115350189d6263b
arutyunyan <arutyunyan@bolid.ru>
2022-08-10 17:59:22 +0300

    Выношу отдельно обработку переменных используемые в tsml шаблонах

---
+++ src/dom/Variable.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace suql\dom;
+
+/**
+ * Класс управления переменными в шаблоне
+ * 
+ * @author Yuriy Arutyunyan <sagittaracc@gmail.com>
+ */
+class Variable
+{
+    /**
+     * @var string имя переменной
+     */
+    private $name;
+    /**
+     * Constructor
+     * @param string $name имя переменной
+     */
+    function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
