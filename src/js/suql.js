fceaa092b476f846318208190a466ce59f8470ed
arutyunyan <arutyunyan@bolid.ru>
2022-08-12 15:47:07 +0300

    Первый переделанный вариант suql.js

--- src/js/suql.js
+++ src/js/suql.js
@@ -6,66 +6,46 @@ function getElementsByQuery(query, add = false) {
     }
     var className = queryParts[1]
     var elements = document.getElementById(id).getElementsByClassName(className)
-    if (elements.length === 0 && add) {
-        var element = document.createElement('div')
-        element.classList = className
-        elements = [document.getElementById(id).appendChild(element)]
-    }
     return elements
 }
 
-function assign(a, b)
-{
+function assign(a, b) {
     a.value = typeof b === "function" ? b(a.value) : b
-    var elements = getElementsByQuery(a.path)
 
-    if (elements.length === 0) {
-        return
-    }
-    
-    if (typeof a.value === "object") {
-        var content = "";
-        for(var i = 0, n = a.value.length; i < n; i++) {
-            var template = a.template;
-            for (variable in a.value[i]) {
-                template = template.replace("[[" + variable + "]]", a.value[i][variable])
+    for (var path in a.paths) {
+        var elements = getElementsByQuery(path)
+
+        if (elements.length === 0) {
+            return
+        }
+
+        var config = a.paths[path]
+
+        for (var i = 0, n = elements.length; i < n; i++) {
+            switch (config.format) {
+                case "raw":
+                    elements[i].textContent = a.value
+                    break
+                case "value":
+                    elements[i].value = a.value
+                    break
+                case "html":
+                    var content = "";
+                    for (var i = 0, n = a.value.length; i < n; i++) {
+                        var template = config.template;
+                        for (variable in a.value[i]) {
+                            template = template.replace("[[" + variable + "]]", a.value[i][variable])
+                        }
+                        content += template
+                    }
+
+                    for (var i = 0, n = elements.length; i < n; i++) {
+                        elements[i].innerHTML = content
+                    }
+                    break
             }
-            content += template
-        }
-
-        for (var i = 0, n = elements.length; i < n; i++) {
-            elements[i].innerHTML = content
-        }
-    }
-    else {
-        for (var i = 0, n = elements.length; i < n; i++) {
-            elements[i].textContent = a.value
         }
     }
 
     // TODO: обновить все связи по template functions которые используют данную переменную
-}
-
-function append(a, b)
-{
-    var elements = getElementsByQuery(a.path, true);
-
-    if (typeof a.value === "object") {
-        a.value.push(b)
-        var template = a.template
-        for (variable in b) {
-            template = template.replace("[[" + variable + "]]", b[variable])
-        }
-
-        for (var i = 0, n = elements.length; i < n; i++) {
-            elements[i].innerHTML += template;
-        }
-    }
-
-    // TODO: обновить все связи по template functions которые используют данную переменную
-}
-
-function get(a)
-{
-    return a.value
 }
